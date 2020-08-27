12019cdc7284ace1c4feb1374b29e068e16c6010
Junho Choi <junhochoi13@gmail.com>
2020-08-27 13:37:23 +0900

    Fix light sound & Add hand interaction for end & restart

--- js/finger_with_light.js
+++ js/finger_with_light.js
@@ -29,7 +29,7 @@ document.getElementById('img_container').style.backgroundColor = '#808080';
 const sfx = new AudioContext();
 sfx = new Audio('sound/tightening_sound_effect.m4a');*/
 //var sfx = new Audio('sound/tightening_sound_effect.m4a');
-var sfx = new Audio('sound/tsf_alt.mp3');
+var sfx = new Audio('sound/tsf.m4a');
 var end_sound = new Audio('sound/ding.m4a');
 
 var introvideo = document.getElementById('watchMe');
@@ -48,6 +48,30 @@ introvideo.addEventListener('timeupdate', function(){
 var c;
 var ctx;
 var light_img_array = ['img/light_left.jpeg', 'img/light_right.jpeg', 'img/light_on.jpeg']
+
+// NEW CODE from here
+var repeat;
+var timer;
+var end_ex = false;
+var timer_start = false;
+function endEx(txt) {
+  progress = 0 // out of 5 for five seconds
+  timer_start = true;
+  var t = new Date();
+  t.setSeconds(t.getSeconds() + 5);
+  timer = setInterval(function() {
+    var delta = t - Date.now();
+    progress++;
+    console.log(progress);
+    //console.log(delta);
+    if (delta<0) {
+      clearInterval(timer);
+      clearInterval(repeat); // end loop
+      alert(txt);
+    };
+  },1000);
+}
+// TO HERE
 window.onload = () => {
   sfx.load();
   end_sound.load();
@@ -71,37 +95,71 @@ window.onload = () => {
   var count = 0; //count
   var thumb_on_left = true;//bool for checking whether motion left or right
 
+  var vid = document.getElementById('video');
+
+  var end_select = false;
   // don't run redrawing until model is set up
   //setTimeout(function() {
-    var repeat = setInterval(function(){
+
+  repeat = setInterval(function(){
       if (start == true) {
         var last_el = trainingData[trainingData.length-1];
 
         //Only for RIGHT HAND
         var num = (last_el["thumb4"][0]-last_el["palm"][0])
 
-        if (count == 5) {
+        if (count == 5 && end_ex == false) {
+          end_ex = true; // exercise is completed
           end_sound.play();
           document.getElementById('img_container').style.background = '#FFEC69';
           document.getElementById('popup_box').style.display = 'block';
           draw_img(light_img_array[2]);
           //alert("Exercise completed");
-          clearInterval(repeat);
+          //clearInterval(repeat); // end loop
         }
-        else if (num>0 && thumb_on_left==false) {
+        else if (num>0 && thumb_on_left==false && end_ex==false) {
           sfx.pause();
-          sfx.currentTime = 0;
           sfx.play();
           draw_img(light_img_array[0]);
           thumb_on_left = true;
         }
-        else if (num<0 && thumb_on_left==true) {
+        else if (num<0 && thumb_on_left==true && end_ex==false) {
           sfx.pause();
-          sfx.currentTime = 0;
           sfx.play();
           draw_img(light_img_array[1]);
           thumb_on_left = false;
           count++;
+        }
+        // NEW CODE NEW CODE
+        // Interaction to be carried out when exercise is completed
+        else if (end_ex == true) {
+          var mid = (last_el["bb"][0] + last_el["bb"][2])/2 // start calculating mid
+          // RIGHT HAND SIDE INTERACTION
+          if (mid<(vid.videoWidth/2)) {
+            // Evaluate if side is coming from left (End button selected)
+            if (end_select == true) {
+              console.log('INTERVAL TIMER IS CLEARED');
+              clearInterval(timer);
+              timer_start = false;
+            }
+            end_select = false;
+            if (timer_start == false) {
+              endEx('Restart exercise');
+            }
+          }
+          // LEFT HAND SIDE INTERACTION
+          else if (mid>(vid.videoWidth/2)) {
+            // Evaluate if side is coming from right (Restart button selected)
+            if (end_select == false) {
+              console.log('INTERVAL TIMER IS CLEARED');
+              clearInterval(timer);
+              timer_start = false;
+            }
+            end_select = true;
+            if (timer_start == false) {
+              endEx('End exercise');
+            }
+          }
         }
       }
     }, 500);
